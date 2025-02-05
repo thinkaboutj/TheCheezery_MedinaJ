37dc96c2a12befef29e27a9e1845aa831e5eb07b
Jesus <159750556+thinkaboutj@users.noreply.github.com>
2025-02-04 21:28:16 -0700

    tutorial completa falta correr y ver si tiene errores

--- TheCheezery/app/src/main/java/medina/jesus/thecheezery/MainActivity.kt
+++ TheCheezery/app/src/main/java/medina/jesus/thecheezery/MainActivity.kt
@@ -1,6 +1,8 @@
 package medina.jesus.thecheezery
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +13,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+
+        val button:Button = findViewById(R.id.button_get_started) as Button
+
+        button.setOnClickListener{
+            var intent: Intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
         }
     }
 }
--- TheCheezery/app/src/main/java/medina/jesus/thecheezery/RegisterActivity.kt
+++ TheCheezery/app/src/main/java/medina/jesus/thecheezery/RegisterActivity.kt
@@ -1,6 +1,8 @@
 package medina.jesus.thecheezery
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -10,11 +12,12 @@ class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_register)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val button:Button = findViewById(R.id.button_sign_in) as Button
+
+        button.setOnClickListener{
+            var intent: Intent = Intent(this, MenuActivity::class.java)
+            startActivity(intent)
         }
     }
 }

d2b85fe5ccb30dd96852b5ed3b792ede313d5d76
Jesus <159750556+thinkaboutj@users.noreply.github.com>
2025-02-11 22:07:25 -0700

    ahi va, solo me falta que los menus se vean como en  el docs

--- TheCheezery/app/src/main/java/medina/jesus/thecheezery/MenuActivity.kt
+++ TheCheezery/app/src/main/java/medina/jesus/thecheezery/MenuActivity.kt
@@ -15,9 +15,30 @@ class MenuActivity : AppCompatActivity() {
         setContentView(R.layout.activity_menu)
 
         var btnCold: Button = findViewById<Button>(R.id.button_cold_drinks)
+        var btnHot: Button = findViewById<Button>(R.id.button_hot_drinks)
+        var btnSweets: Button = findViewById<Button>(R.id.button_sweets)
+        var btnSalties: Button = findViewById<Button>(R.id.button_salties)
 
-        btnCold.setOnClickListener {
-            var intent: Intent = Intent(this, ProductosActivity::class.java)
+
+        //tuve que cambiar todo para implementar los demas xd
+        btnCold.setOnClickListener{
+            var intent: Intent = Intent(this,ProductosActivity::class.java)
+            intent.putExtra("menuType","coldDrinks")
+            startActivity(intent)
+        }
+        btnHot.setOnClickListener{
+            var intent: Intent = Intent(this,ProductosActivity::class.java)
+            intent.putExtra("menuType","hotDrinks")
+            startActivity(intent)
+        }
+        btnSweets.setOnClickListener{
+            var intent: Intent = Intent(this,ProductosActivity::class.java)
+            intent.putExtra("menuType","sweets")
+            startActivity(intent)
+        }
+        btnSalties.setOnClickListener{
+            var intent: Intent = Intent(this,ProductosActivity::class.java)
+            intent.putExtra("menuType","salties")
             startActivity(intent)
         }
     }
--- TheCheezery/app/src/main/java/medina/jesus/thecheezery/ProductosActivity.kt
+++ TheCheezery/app/src/main/java/medina/jesus/thecheezery/ProductosActivity.kt
@@ -16,69 +16,94 @@ import androidx.core.view.WindowInsetsCompat
 import androidx.lifecycle.findViewTreeViewModelStoreOwner
 
 class ProductosActivity : AppCompatActivity() {
-
-    var coldDrinks = ArrayList<Product>()
-
+    var menu=ArrayList<Product>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_productos)
+        var menuOption:String? = intent.getStringExtra("menuType")
+        agregarProducto(menuOption)
 
-        agregarProductos()
-
-        var listview: ListView = findViewById<ListView>(R.id.listview)
-
-        var adaptador: AdaptadorProductos = AdaptadorProductos(this,coldDrinks)
-
-        listview.adapter = adaptador
+        var listView: ListView = findViewById(R.id.listview) as ListView
+        var adaptador : AdaptadorProductos= AdaptadorProductos(this,menu)
+        listView.adapter=adaptador
     }
 
-    fun agregarProductos(){
-        coldDrinks.add(Product("caramel Frap", R.drawable.caramelfrap, "Caramel syrup meets coffee, milk and ice and whipped cream and buttery caramel sauce layer the love on top.", 5.0))
-        coldDrinks.add(Product("Chocolate Frap", R.drawable.chocolatefrap, "Rich mocha-flavored sauce meets up with chocolaty chips, milk and ice for a blender bash.", 6.0))
-        coldDrinks.add(Product("Cold Brew", R.drawable.coldbrew, "Created by steeping medium-to-coarse ground coffee in room temperature water for 12 hours or longer.", 3.0))
-        coldDrinks.add(Product("Matcha Latte", R.drawable.matcha, "Leafy taste of matcha green tea powder with creamy milk and a little sugar for a flavor balance that will leave you feeling ready and raring to go.", 4.0))
-        coldDrinks.add(Product("Oreo Milkshake", R.drawable.oreomilkshake, "Chocolate ice cream, and oreo cookies. Topped with whipped cream with cocoa and chocolate syrup.", 7.0))
-        coldDrinks.add(Product("Peanut Milkshake", R.drawable.peanutmilkshake, "Vanilla ice cream, mixed with peanut butter and chocolate.", 7.0))
+    fun agregarProducto(option:String?){
+        when(option){
+            "coldDrinks"->{
+                var coldDrinks =  ArrayList<Product>()
+                menu.add(Product("Caramel Frap", R.drawable.caramelfrap, "Caramel syrup meets coffee, milk and ice and whipped cream and buttery caramel sauce layer the love on top.", 5.0))
+                menu.add(Product("Chocolate Frap", R.drawable.chocolatefrap, "Rich mocha-flavored sauce meets up with chocolaty chips, milk and ice for a blender bash.", 6.0))
+                menu.add(Product("Cold Brew", R.drawable.coldbrew, "Created by steeping medium-to-coarse ground coffee in room temperature water for 12 hours or longer.", 3.0))
+                menu.add(Product("Matcha Latte", R.drawable.matcha, "Leafy taste of matcha green tea powder with creamy milk and a little sugar for a flavor balance that will leave you feeling ready and raring to go.", 4.0))
+                menu.add(Product("Oreo Milkshake", R.drawable.oreomilkshake, "Chocolate ice cream, and oreo cookies. Topped with whipped cream with cocoa and chocolate syrup.", 7.0))
+                menu.add(Product("Peanut Milkshake", R.drawable.peanutmilkshake, "Vanilla ice cream, mixed with peanut butter and chocolate.", 7.0))
+            }
+            "hotDrinks"->{
+                var hotDrinks = ArrayList<Product>()
+                menu.add(Product("Latte", R.drawable.latte, "Coffee drink made with espresso and steamed milk", 6.0))
+                menu.add(Product("Hot chocolate", R.drawable.hotchocolate, "Heated drink consisting of shaved chocolate, topped with marshmallows.", 5.0))
+                menu.add(Product("Espresso", R.drawable.espresso, "Full-flavored, concentrated form of coffee.", 4.0))
+                menu.add(Product("Chai Latte", R.drawable.chailatte, "Spiced tea concentrate with milk", 6.0))
+                menu.add(Product("Capuccino", R.drawable.capuccino, "A cappuccino is an espresso-based coffee drink, prepared with steamed foam.", 7.0))
+                menu.add(Product("American coffee", R.drawable.americano, "Espresso with hot water", 2.0))
 
+            }
+            "sweets"->{
+                var sweets = ArrayList<Product>()
+                menu.add(Product("Blueberry cake", R.drawable.blueberrycake, "Vanilla cake flavor, topped with cheese topping and blueberries.", 6.0))
+                menu.add(Product("Chocolate cupcake", R.drawable.chocolatecupcake, "Chocolate cupcakes topped with butter cream and cherries", 3.0))
+                menu.add(Product("Lemon tartalette", R.drawable.lemontartalette, "Pastry shell with a lemon flavored filling", 4.0))
+                menu.add(Product("Red Velvet cake", R.drawable.redvelvetcake, "Soft, moist, buttery cake topped with an easy cream cheese frosting.", 6.0))
+                menu.add(Product("Cherry cheesecake", R.drawable. strawberrycheesecake, "This cherry topped cheesecake is positively creamy and delicious and will be your new favorite dessert.", 7.0))
+                menu.add(Product("Tiramisu", R.drawable.tiramisu, "Coffee-flavored Italian dessert", 6.0))
+            }
+            "salties"->{
+                var salties = ArrayList<Product>()
+                menu.add(Product("Chicken crepes", R.drawable.chickencrepes, "Fine crepes stuffed with Alfredo chicken, spinach and mushrooms.", 6.0))
+                menu.add(Product("Club Sandwich", R.drawable.clubsandwich, "A delicious sandwich served with french fries.", 5.0))
+                menu.add(Product("Panini", R.drawable.hampanini, "Sandwich made with Italian bread  served warmed by grilling.", 4.0))
+                menu.add(Product("Philly cheese steak", R.drawable. phillycheesesteak, "Smothered in grilled onions, green peppers, mushrooms, and Provolone.", 6.0))
+                menu.add(Product("Nachos", R.drawable. nachos, "Tortilla chips layered with beef and   melted cheddar cheese. Served with fried beans, guacamole, pico de gallo, and sour topping.",  7.0))
+
+            }
+        }
     }
+    private class AdaptadorProductos: BaseAdapter{
+        var producto=ArrayList<Product>()
+        var contexto: Context?=null
 
-    private  class  AdaptadorProductos:BaseAdapter{
-        var productos = ArrayList<Product>()
-        var contexto: Context?= null
-
-        constructor(context: Context, productos: ArrayList<Product>){
-            this.productos = productos
-            this.contexto = contexto
+        constructor(contexto: Context,productos: ArrayList<Product>){
+            this.producto=productos
+            this.contexto=contexto
         }
 
         override fun getCount(): Int {
-            return  productos.size
+            return producto.size
         }
 
-        override fun getItem(position: Int): Any {
-            return productos[position]
+        override fun getItem(p0: Int): Any {
+            return producto[p0]
         }
 
-        override fun getItemId(position: Int): Long {
-            return position.toLong()
+        override fun getItemId(p0: Int): Long {
+            return p0.toLong()
         }
 
-        override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
-            var prod = productos[position]
-            var inflador = LayoutInflater.from(contexto)
-            var vista = inflador.inflate(R.layout.producto_view, null)
+        override fun getView(p0: Int, p1: View?, p2: ViewGroup?): View {
+            var prod=producto[p0]
+            var inflador=LayoutInflater.from(contexto)
+            var vista=inflador.inflate(R.layout.producto_view,null)
 
-            var imagen = vista.findViewById<ImageView>(R.id.producto_img)
-            var nombre = vista.findViewById<TextView>(R.id.producto_nombre)
-            var desc = vista.findViewById<TextView>(R.id.producto_desc)
-            var precio = vista.findViewById<TextView>(R.id.producto_precio)
+            var imagen = vista.findViewById(R.id.producto_img) as ImageView
+            var nombre = vista.findViewById(R.id.producto_nombre) as TextView
+            var desc = vista.findViewById(R.id.producto_desc) as TextView
+            var precio=vista.findViewById(R.id.producto_precio) as TextView
 
             imagen.setImageResource(prod.image)
             nombre.setText(prod.name)
             desc.setText(prod.descripcion)
             precio.setText("$${prod.price}")
-
             return vista
         }
     }

81b8261ec0ae5449326772f180f379226d7f3962
Jesus <159750556+thinkaboutj@users.noreply.github.com>
2025-02-11 21:41:03 -0700

    termine el video ahora le continuare yo

--- TheCheezery/app/src/main/java/medina/jesus/thecheezery/MenuActivity.kt
+++ TheCheezery/app/src/main/java/medina/jesus/thecheezery/MenuActivity.kt
@@ -1,6 +1,8 @@
 package medina.jesus.thecheezery
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +13,12 @@ class MenuActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_menu)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        var btnCold: Button = findViewById<Button>(R.id.button_cold_drinks)
+
+        btnCold.setOnClickListener {
+            var intent: Intent = Intent(this, ProductosActivity::class.java)
+            startActivity(intent)
         }
     }
 }
